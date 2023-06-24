9193e0598d7066e41351c4ef9b44a710cf4a6485
zhaojh <1763124707@qq.com>
2023-06-24 14:42:38 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+	configureWebpack: {
+		resolve: {
+			alias: {
+				'assets': '@/assets',
+				'common': '@/common',
+				'components': '@/components',
+				'api': '@/api',
+				'views': '@/views',
+				'plugins': '@/plugins'
+			}
+		}
+	},
+	devServer: {
+		open: true,           // 是否自动打开项目
+		port: 8089,           // 端口号
+		https: false,         // 把访问变成https
+		hotOnly: false,       // 热更新
+		disableHostCheck: true,   // 解决内网穿透 Invalid Host header问题
+
+	}
+
+}
